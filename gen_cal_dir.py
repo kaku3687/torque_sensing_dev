a987ddc3cb53a7a262e2dcfff7d4b8185ca5fe9d
Thara Randhawa <tsrandha@gmail.com>
2017-12-07 22:28:04 -0800

    Added first working version of calibration directory generator

--- gen_cal_dir.py
+++ gen_cal_dir.py
@@ -1,6 +1,6 @@
 import os, errno
 
-working_dir = "C:/Users/trandhawa/Documents/Robomantis_Cal/"
+working_dir = "C:/Users/Owner/Documents/Robomantis_Cal/"
 
 type_ = ["50009880", "50009900", "50010350"]
 type_1_start = 11
@@ -15,9 +15,24 @@ type_5_num = 4
 
 for i in range(type_1_start, (type_1_num + type_1_start)):
 	dir_name = working_dir + type_[0] + "_00" + str(i) + "_BLK2"
-	file_name = "README_" + type_[0] + "_00" + str(i)
+	file_name = dir_name + "/" + "README_" + type_[0] + "_00" + str(i) + ".txt"
 	rdme_f = open(file_name, "w")
-	
+	rdme_f.write(file_name)
+	rdme_f.write("\n")
+	rdme_f.write("Calibrated On:")
+	rdme_f.write("Calibrated By:")
+	rdme_f.write("\n")
+	rdme_f.write("Commutation Parameters:")
+	rdme_f.write("		CS:")
+	rdme_f.write("		SSI1:")
+	rdme_f.write("\n")
+	rdme_f.write("Nominal Temperature:")
+	rdme_f.write("\n")
+	rdme_f.write("SSI1:")
+	rdme_f.write("		0 Rel:")
+	rdme_f.write("		10000 Rel:")
+	rdme_f.write("		0 Rel:")
+	rdme_f.close()
 
 	try:
 		os.makedirs(dir_name)
